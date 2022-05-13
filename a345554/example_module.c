760d2bbc2c8438c27d07b8f4fad6e580c6fbbe69
Andres Silva Ortega <101672541+Mopetaso@users.noreply.github.com>
2022-05-12 19:02:05 -0600

    Create example_module.c

---
+++ a345554/example_module.c
@@ -0,0 +1,20 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Andres Silva Ortega");
+MODULE_DESCRIPTION("Example Linux Kernel Module.");
+MODULE_VERSION("0.01");
+
+static int __init example_init(void) {
+ printk(KERN_INFO "MODULO INICALIZADO\n");
+ return 0;
+}
+
+static void __exit example_exit(void) {
+ printk(KERN_INFO "MODULO FINALIZADO\n");
+}
+
+module_init(example_init);
+module_exit(example_exit);
