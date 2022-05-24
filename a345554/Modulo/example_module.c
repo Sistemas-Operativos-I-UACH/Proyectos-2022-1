e17d80011c3719a75eb9bdfcfd05afaa102c8b8d
Andres Silva Ortega <andres.sil.ort@gmail.com>
2022-05-24 16:20:38 -0600

    ok

--- a345554/Modulo/example_module.c
+++ a345554/Modulo/example_module.c
@@ -1,19 +1,77 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-
+#include <linux/fs.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Andres Silva Ortega");
 MODULE_DESCRIPTION("Example Linux Kernel Module.");
 MODULE_VERSION("0.01");
 
+#define DEVICE_NAME "example_module"
 
+static int major;
+
+static int dev_open(struct inode*, struct file*);
+static int dev_release(struct inode*, struct file*);
+static ssize_t dev_read(struct file*, char*, size_t, loff_t*);
+static ssize_t dev_write(struct file*, const char*, size_t, loff_t*);
+
+static struct file_operations fops = {
+   .open = dev_open,
+   .read = dev_read,
+   .write = dev_write,
+   .release = dev_release,
+};
 
 static int __init example_init(void) {
- printk(KERN_INFO "MODULO INICALIZADO\n");
- return 0;
+    major = register_chrdev(0, DEVICE_NAME, &fops);
+
+    if (major < 0) {
+        printk(KERN_ALERT "Module load failed\n");
+        return major;
+    }
+
+    printk(KERN_INFO "Module Started!!!\n");
+    printk(KERN_INFO "I was assigned major number %d. To talk to\n", major);
+    printk(KERN_INFO "the driver, create a dev file with\n");
+    printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n", DEVICE_NAME, major);
+    printk(KERN_INFO "Try various minor numbers. Try to cat and echo to\n");
+    printk(KERN_INFO "the device file.\n");
+    printk(KERN_INFO "Remove the device file and module when done.\n");
+
+    return 0;
 }
+
+
+static int dev_open(struct inode *inodep, struct file *filep) {
+   printk(KERN_INFO "Module device opened\n");
+   return 0;
+}
+
+static ssize_t dev_write(struct file *filep, const char *buffer,
+                         size_t len, loff_t *offset) {
+
+   printk(KERN_INFO "Sorry, Module is read only\n");
+   return -EFAULT;
+}
+
+static int dev_release(struct inode *inodep, struct file *filep) {
+   printk(KERN_INFO "Module device closed\n");
+   return 0;
+}
+static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset) {
+    int errors = 0;
+    char *message = " - - Kuriki taka ti, kuriki taka ta - - ";
+    int message_len = strlen(message);
+
+    errors = copy_to_user(buffer, message, message_len);
+
+    return errors == 0 ? message_len : -EFAULT;
+    return 0;
+}
+
+
 
 static void __exit example_exit(void) {
  printk(KERN_INFO "MODULO FINALIZADO\n");
